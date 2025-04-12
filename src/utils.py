a781fcb3a5597d92a1f02a9b93f2a5e9c53a66eb
PEI ZENG <zplance0916@gmail.com>
2025-04-12 13:03:16 -0400

    [Fix] Fix the file update bug

--- src/utils.py
+++ src/utils.py
@@ -21,9 +21,8 @@ def save_three_values(df1, df2, df3, path):
 
     visualization_df = df1.merge(
         df2, how = "left", left_on=config.LOC, right_on=config.LOC).merge(df3, how = "left", left_on=config.LOC, right_on=config.LOC)
-    visualization_df.columns = ['lon_bin', 'lat_bin', 'yesterday_pred', 'yesterday_date', 'yesterday_true', 'today_date', 'today_pred']
+    visualization_df.columns = ['lon_bin', 'lat_bin', 'yesterday_date', 'yesterday_pred', 'yesterday_true', 'today_date', 'today_pred']
     
     visualization_df['yesterday_diff'] = visualization_df['yesterday_true'] - visualization_df['yesterday_pred']
-    visualization_df = visualization_df[['lon_bin', 'lat_bin', 'yesterday_date', 'yesterday_pred', 'yesterday_true', 'today_date', 'today_pred', 'yesterday_diff']]
     visualization_df.to_csv(path, index = False)
 
